ad96fb15613e64e22348edf7ba8d4017381b78ea
haward79 <haward79@yahoo.com.tw>
2022-02-18 17:25:15 +0800

    - 02 / 18 2022   - First commit.   - Program is ready to use.

---
+++ README.md
@@ -0,0 +1,44 @@
+# What's this
+This is a news push program.  
+This program collect news from https://infonews.nycu.edu.tw/ and the news will send to specific email address.
+
+# Pre-requirements
+
+- Software
+  - Linux based OS
+    - *Note: This program may run on Windows by modifying some codes.*
+  - Python 3
+  - Python 3 Library - feedparser, smtplib, email
+    - *Note: You can install them by pip.*
+  - Mail Trasfer Agent
+  - cron
+
+# Install
+
+## Install cron
+Please install main.py to cron.
+
+Example config:
+```
+# m  h  dom mon dow   command
+  0  *   *   *   *    python3 /home/user/Routine/nycu_news/main.py
+```
+
+## Change mail info
+Use text editor to open main.py and edit the following code:
+```
+mail['To'] = 'user@example.com'
+mail['From'] = 'sender@example.com'
+```
+Please change 'user@example.com' and 'sender@example.com' to the correct mail sender and receiver.
+
+
+# Changelog
+- 02 / 18 2022
+  - First commit.
+  - Program is ready to use.
+
+# Copyright
+This program is written by [haward79](https://www.haward79.tw/).  
+All rights reserved by haward79.
+
---
+++ main.py
@@ -0,0 +1,404 @@
+
+from datetime import datetime
+from datetime import timedelta
+import requests
+from bs4 import BeautifulSoup
+import smtplib
+from email.message import EmailMessage
+
+categoryBgcolorClass = {
+    '行政': 'bg1',
+    '演講': 'bg2',
+    '學術': 'bg3',
+    '招生': 'bg4',
+    '徵才': 'bg5',
+    '校園活動': 'bg6',
+    '藝文活動': 'bg7',
+    '其他': 'bg8'
+}
+
+
+def get_categoryName(imageUrl):
+
+    if(imageUrl == 'images/bt01.png'):
+        name = '行政'
+    
+    elif(imageUrl == 'images/bt02.png'):
+        name = '演講'
+
+    elif(imageUrl == 'images/bt03.png'):
+        name = '學術'
+
+    elif(imageUrl == 'images/bt04.png'):
+        name = '招生'
+
+    elif(imageUrl == 'images/bt05.png'):
+        name = '徵才'
+
+    elif(imageUrl == 'images/bt06.png'):
+        name = '校園活動'
+
+    elif(imageUrl == 'images/bt07.png'):
+        name = '藝文活動'
+
+    elif(imageUrl == 'images/bt09.png'):
+        name = '其他'
+
+    else:
+        name = ''
+
+    return name
+
+
+def fetchNews():
+
+    news = []
+    today = datetime.now().strftime('%Y %m.%d')
+    yesterday = (datetime.now() - timedelta(days=1)).strftime('%Y %m.%d')
+    pageNo = 1
+
+    while pageNo <= 10:
+        url = 'https://infonews.nycu.edu.tw/index.php?SuperType=2&action=more&categoryid=all&pagekey=' + str(pageNo)
+        sourceCode = requests.get(url, headers={"User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:98.0) Gecko/20100101 Firefox/98.0"}).content
+        sourceCode.decode('big5')
+        rows = BeautifulSoup(sourceCode, 'html.parser').find('div', {'id': 'layout_more'}).findAll('tr')
+        rowsLength = len(rows)
+
+        for i in range(rowsLength):
+            row = rows[i]
+            
+            if row.find('img', {'class': 'style1'}) != None:
+                category = ''
+                title = ''
+                link = ''
+                dateStart = ''
+                dateEnding = ''
+                publisher = ''
+
+                # Fetch category.
+                category = get_categoryName(row.find('img', {'class': 'style1'})['src'])
+                
+                # Fetch title and link.
+                element = row.find('td', {'class': 'style2'})
+
+                if element != None:
+                    element = element.find('a')
+
+                    if element != None:
+                        title = element.contents[0]
+
+                        link = element['href']
+                        link = 'https://infonews.nycu.edu.tw/' + link
+
+                # Fetch date.
+                if i + 1 < rowsLength:
+                    element = rows[i+1].find('td', {'class': 'style4'})
+
+                    if element != None:
+                        date = element.contents[0]
+                        dateSepIndex = date.find('-')
+
+                        if dateSepIndex != -1:
+                            dateStart = date[:dateSepIndex]
+                            dateStart = datetime.strptime(dateStart, '%Y/%m/%d')
+                            dateStartStr = dateStart.strftime('%Y %m.%d')
+
+                            dateEnding = date[dateSepIndex+1:]
+                            dateEnding = datetime.strptime(dateEnding, '%Y/%m/%d')
+                            dateEndingStr = dateEnding.strftime('%Y %m.%d')
+
+                # Fetch publisher.
+                if i + 2 < rowsLength:
+                    element = rows[i+2].find('td', {'class': 'style4'})
+
+                    if element != None:
+                        publisher = element.contents[0]
+
+                # Check start date.
+                if dateStartStr == yesterday:
+                    news.append({
+                        'category': category,
+                        'title': title,
+                        'link': link,
+                        'dateStart': dateStartStr,
+                        'dateEnding': dateEndingStr,
+                        'publisher': publisher
+                    })
+
+        pageNo += 1
+
+    return news
+
+
+def fetchActivity():
+
+    news = []
+    today = datetime.now().strftime('%Y %m.%d')
+    yesterday = (datetime.now() - timedelta(days=1)).strftime('%Y %m.%d')
+    pageNo = 1
+
+    while pageNo <= 10:
+        url = 'https://infonews.nycu.edu.tw/index.php?SuperType=1&action=more&categoryid=all&pagekey=' + str(pageNo)
+        sourceCode = requests.get(url, headers={"User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:98.0) Gecko/20100101 Firefox/98.0"}).content
+        sourceCode.decode('big5')
+        rows = BeautifulSoup(sourceCode, 'html.parser').find('div', {'id': 'layout_more'}).findAll('tr')
+        rowsLength = len(rows)
+
+        for i in range(rowsLength):
+            row = rows[i]
+            
+            if row.find('img', {'class': 'style1'}) != None:
+                category = ''
+                title = ''
+                link = ''
+                dateStart = ''
+                dateEnding = ''
+                publisher = ''
+
+                # Fetch category.
+                category = get_categoryName(row.find('img', {'class': 'style1'})['src'])
+                
+                # Fetch title and link.
+                element = row.find('td', {'class': 'style2'})
+
+                if element != None:
+                    element = element.find('a')
+
+                    if element != None:
+                        title = element.contents[0]
+
+                        link = element['href']
+                        link = 'https://infonews.nycu.edu.tw/' + link
+
+                # Fetch date.
+                if i + 1 < rowsLength:
+                    element = rows[i+1].find('td', {'class': 'style4'})
+
+                    if element != None:
+                        date = element.contents[0]
+                        dateSepIndex = date.find('-')
+
+                        if dateSepIndex != -1:
+                            dateStart = date[:dateSepIndex]
+                            dateStart = datetime.strptime(dateStart, '%Y/%m/%d')
+                            dateStartStr = dateStart.strftime('%Y %m.%d')
+
+                            dateEnding = date[dateSepIndex+1:]
+                            dateEnding = datetime.strptime(dateEnding, '%Y/%m/%d')
+                            dateEndingStr = dateEnding.strftime('%Y %m.%d')
+
+                # Fetch publisher.
+                if i + 2 < rowsLength:
+                    element = rows[i+2].find('td', {'class': 'style4'})
+
+                    if element != None:
+                        publisher = element.contents[0]
+
+                # Check start date.
+                if dateStartStr == yesterday:
+                    news.append({
+                        'category': category,
+                        'title': title,
+                        'link': link,
+                        'dateStart': dateStartStr,
+                        'dateEnding': dateEndingStr,
+                        'publisher': publisher
+                    })
+
+        pageNo += 1
+
+    return news
+
+
+def sendMail(news, activities):
+
+    if len(news) > 0:
+        mailContent = """
+        <html>
+            <body>
+                <header>
+                    <h1>交大公告推播</h1>
+                </header>
+                
+                <table>
+                    <tr>
+                        <th class="news_category_cell" colspan="2">公告</th>
+                        <th class="news_published_cell">發布時間</th>
+                    </tr>
+        """
+
+        for item in news:
+            mailContent += '<tr><td class="news_category_cell"><span class="news_category_text ' + categoryBgcolorClass[item['category']] + '">' + item['category'] + '</span></td>'
+            mailContent += '<td class="news_title_cell"><a href="' + item['link'] + '">' + item['title'] + '</a></td><td class="news_published_cell">' + item['dateStart'] + '</td></tr>'
+
+        mailContent += """
+                </table>
+
+                <div class="space_medium"></div>
+
+                <header>
+                    <h1>交大活動推播</h1>
+                </header>
+                
+                <table>
+                    <tr>
+                        <th class="news_category_cell" colspan="2">公告</th>
+                        <th class="news_published_cell">發布時間</th>
+                    </tr>
+        """
+
+        for item in activities:
+            mailContent += '<tr><td class="news_category_cell"><span class="news_category_text ' + categoryBgcolorClass[item['category']] + '">' + item['category'] + '</span></td>'
+            mailContent += '<td class="news_title_cell"><a href="' + item['link'] + '">' + item['title'] + '</a></td><td class="news_published_cell">' + item['dateStart'] + '</td></tr>'
+
+        mailContent += """
+                </table>
+
+                <div class="space_medium"></div>
+
+                <style>
+
+                    body
+                    {
+                        margin: 0px auto;
+                        width: 1024px;
+                    }
+
+                    header h1
+                    {
+                        margin: 20px 0px 40px;
+                        font-size: 30px;
+                        letter-spacing: 5px;
+                        text-align: center;
+                    }
+
+                    a:link,
+                    a:visited
+                    {
+                        border-bottom: 1px solid transparent;
+                        color: #000000;
+                        text-decoration: none;
+                    }
+
+                    a:hover,
+                    a:active
+                    {
+                        border-bottom: 1px solid #000000;
+                    }
+
+                    table
+                    {
+                        width: 100%;
+                        border-collapse: collapse;
+                    }
+
+                    th, td
+                    {
+                        padding: 10px;
+                        font-size: 20px;
+                        font-weight: normal;
+                    }
+
+                    th
+                    {
+                        border-bottom: 1px solid #000000;
+                        letter-spacing: 5px;
+                    }
+
+                    td
+                    {
+                        border-bottom: 1px solid #a4a3a3;
+                    }
+
+                    .news_category_cell
+                    {
+                        padding-right: 0px;
+                        width: 70px;
+                    }
+
+                    .news_category_text
+                    {
+                        padding: 5px 10px;
+                        background-color: #505050;
+                        border-radius: 10px;
+                        color: #ffffff;
+                        font-size: 18px;
+                    }
+
+                    .news_title_cell
+                    {
+                        line-height: 1.7;
+                    }
+
+                    .news_published_cell
+                    {
+                        width: 130px;
+                        text-align: center;
+                    }
+
+                    .bg1
+                    {
+                        background-color: #C425A3;
+                    }
+
+                    .bg2
+                    {
+                        background-color: #F9A02B;
+                    }
+
+                    .bg3
+                    {
+                        background-color: #B8980A;
+                    }
+
+                    .bg4
+                    {
+                        background-color: #2EBA3E;
+                    }
+
+                    .bg5
+                    {
+                        background-color: #2C9DA7;
+                    }
+
+                    .bg6
+                    {
+                        background-color: #2C48A7;
+                    }
+
+                    .bg7
+                    {
+                        background-color: #D82F2F;
+                    }
+
+                    .bg8
+                    {
+                        background-color: #8F3A84;
+                    }
+
+                    .space_medium
+                    {
+                        height: 200px;
+                    }
+
+                </style>
+            </body>
+        </html>
+        """
+
+        mail = EmailMessage()
+        mail.set_content(mailContent, subtype='html')
+        mail['To'] = 'user@example.com'
+        mail['From'] = 'sender@example.com'
+        mail['Subject'] = '交大公告與活動推播 (僅蒐集昨天發布的訊息)'
+
+        mailServer = smtplib.SMTP('localhost')
+        mailServer.send_message(mail)
+        mailServer.quit()
+
+
+news = fetchNews()
+activities = fetchActivity()
+
+sendMail(news, activities)
+
